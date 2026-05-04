0155afafd41051867ade1659aed8d8f6c5765b3d
Soguel-Aurelien <soguel.aurelien08@outlook.com>
2026-05-04 10:11:08 +0200

    Version 2.0.0

--- VersionAenderung/numberGuessing.java
+++ VersionAenderung/numberGuessing.java
@@ -6,29 +6,49 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+        boolean playAgain = true;
 
-        int secret = random.nextInt(100) + 1; 
-        int guess = 0;
-        int guessCounter = 0;
+        while (playAgain) {
 
-        System.out.println("Guess the number (between 1 and 100) :");
+            int secret = random.nextInt(100) + 1;
+            int guess = 0;
+            int guessCounter = 0;
+            int score = 0;
 
-        while (guess != secret) {
-            System.out.print("Your choice : ");
-            guess = scanner.nextInt();
+            System.out.println("Guess the number (between 1 and 100) :");
 
-            System.out.println("Your number of Guess : ");
-            guessCounter++;
+            while (guess != secret) {
+                System.out.print("Your choice : ");
+                guess = scanner.nextInt();
 
-            if (guess < secret) {
-                System.out.println("It's bigger !");
-            } else if (guess > secret) {
-                System.out.println("It's lower !");
-            } else {
-                System.out.println("Congrates, you guessed right !");
+                System.out.println("Your number of Guess : ");
+                guessCounter++;
+
+                if (guess < secret) {
+                    System.out.println("It's bigger !");
+                } else if (guess > secret) {
+                    System.out.println("It's lower !");
+                } else {
+                    System.out.println("Congrates, you guessed right !");
+                    score++;
+                }
             }
-        }
+            scanner.nextLine();
 
-        scanner.close();
+            System.out.println("Do you want to continue playing? Yes or No");
+            String continuePlaying = scanner.nextLine();
+
+            if (continuePlaying.equalsIgnoreCase("Yes")) {
+                playAgain = true;
+                System.out.println("Your current score : "+score);
+            } else if (continuePlaying.equalsIgnoreCase("No")) {
+                playAgain = false;
+                System.out.println("You ended with the score : "+score);
+            } else {
+                System.out.println("Invalid Input");
+            }
+
+            scanner.close();
+        }
     }
 }
